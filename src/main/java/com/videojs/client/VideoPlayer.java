9cf29b6d971bf3871f470c27270fd0d3ff8995e8
abashev <abashev@gmail.com>
2011-01-16 00:51:07 +0300

    First stub for the project. Without any customizations from 'Getting Started' guide.

---
+++ src/main/java/com/videojs/client/VideoPlayer.java
@@ -0,0 +1,37 @@
+package com.videojs.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Base class for the video player. Can be used as ordinary widget.
+ *
+ * @author <A href="mailto:alexey at abashev dot ru">Alexey Abashev</A>
+ * @version $Id$
+ */
+public class VideoPlayer extends Composite {
+    private static VideoPlayerUiBinder uiBinder = GWT.create(VideoPlayerUiBinder.class);
+
+    interface VideoPlayerUiBinder extends UiBinder<Widget, VideoPlayer> {
+    }
+
+    public VideoPlayer() {
+        final Widget widget = uiBinder.createAndBindUi(this);
+
+        initWidget(widget);
+    }
+
+    /* (non-Javadoc)
+     * @see com.google.gwt.user.client.ui.Widget#onLoad()
+     */
+    @Override
+    protected void onLoad() {
+        setupAllWhenReady();
+    }
+
+    public native void setupAllWhenReady() /*-{
+        $wnd.VideoJS.setupAllWhenReady();
+    }-*/;
+}
