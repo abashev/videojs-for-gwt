fdf149f3c3bfade4b3abb03a4bb8e5dd82efba75
Alexey Abashev <alexey@abashev.ru>
2012-06-01 21:48:02 +0400

    Move resources and classes from sandbox to 'player' artifact

--- player/src/main/java/com/videojs/client/VideoPlayer.java
+++ player/src/main/java/com/videojs/client/VideoPlayer.java
@@ -1,8 +1,14 @@
 package com.videojs.client;
 
+import static com.videojs.client.VideoPlayerResources.RESOURCES;
+
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.MediaElement;
+import com.google.gwt.dom.client.SourceElement;
+import com.google.gwt.dom.client.VideoElement;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -11,16 +17,42 @@ import com.google.gwt.user.client.ui.Widget;
  * @author <A href="mailto:alexey at abashev dot ru">Alexey Abashev</A>
  * @version $Id$
  */
-public class VideoPlayer extends Composite {
-    private static VideoPlayerUiBinder uiBinder = GWT.create(VideoPlayerUiBinder.class);
+public class VideoPlayer extends Widget {
+    private static final String FALLBACK_SWF = GWT.getModuleBaseURL() + "/videojs/video-js-3.2.0.swf";
 
-    interface VideoPlayerUiBinder extends UiBinder<Widget, VideoPlayer> {
-    }
+    private String playerId;
 
-    public VideoPlayer() {
-        final Widget widget = uiBinder.createAndBindUi(this);
+    public VideoPlayer(int width, int height, String source) {
+        RESOURCES.css().ensureInjected();
 
-        initWidget(widget);
+        setFlashFallback(FALLBACK_SWF);
+
+        final Document doc = Document.get();
+
+        DivElement container = doc.createDivElement();
+
+        setElement(container);
+
+        VideoElement videoElem = doc.createVideoElement();
+
+        videoElem.setId(playerId = doc.createUniqueId());
+        videoElem.addClassName("video-js");
+        videoElem.addClassName("vjs-default-skin");
+        videoElem.setWidth(width);
+        videoElem.setHeight(height);
+        videoElem.setControls(true);
+        videoElem.setPreload(MediaElement.PRELOAD_NONE);
+
+        videoElem.setPoster("http://video-js.zencoder.com/oceans-clip.png");
+
+        SourceElement srcElem = doc.createSourceElement();
+
+        srcElem.setSrc(source);
+        srcElem.setType("video/mp4");
+
+        videoElem.appendChild(srcElem);
+
+        container.appendChild(videoElem);
     }
 
     /* (non-Javadoc)
@@ -28,10 +60,16 @@ public class VideoPlayer extends Composite {
      */
     @Override
     protected void onLoad() {
-        setupAllWhenReady();
+        initPlayer(playerId);
     }
 
-    public native void setupAllWhenReady() /*-{
-        $wnd.VideoJS.setupAllWhenReady();
+    private native void setFlashFallback(String swfUrl) /*-{
+        $wnd._V_.options.flash.swf = swfUrl;
+    }-*/;
+
+    private native void initPlayer(String videoId) /*-{
+        $wnd._V_(videoId, {}, function() {
+            $wnd.window.alert("Player initialized");
+        });
     }-*/;
 }
